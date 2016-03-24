e537bcaf9b7031a081668b48eded9eb17e7ea965
Vaibhav Bhagee <vaibhavbhagee@gmail.com>
2016-03-24 23:01:25 +0530

    Authentication using Tokens and Login API added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var mongo = require('mongodb').MongoClient;
 var app = express();
 var fs = require("fs");
 var jwt = require("jsonwebtoken");
+var crypto = require('crypto');
 
 var secret = "vaibhavshreyanaayancop290assignment2";
 
@@ -26,6 +27,8 @@ var server = app.listen(port, function () {
 
 })
 
+var apiRoutes = express.Router();
+
 /**
  * 	HTTP-POST REQUEST
  */
@@ -33,10 +36,96 @@ mongo.connect('mongodb://127.0.0.1/complaint_system', function(err,db) {
 	if(err) throw err;
 
   // Collections in the database
+  var users = db.collection("users");
+  var special_users = db.collection("special_users");
+  var complaints = db.collection("complaints");
+  var notifications = db.collection("notifications");
 
   // API definitions
   app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
   });
+
+  apiRoutes.get('/', function(req, res) {
+    res.send({ message: 'Welcome to the coolest API on earth!' });
+  });
+
+  // API for authentication
+
+  apiRoutes.post('/authenticate',function(req,res)
+  {
+    var username = req.body.username;
+    var password = crypto.createHash('sha1').update(req.body.password).digest('hex');
+
+    users.find({"unique_id":username,"password":password}).toArray(function(err,result)
+    {
+      if (err)
+        throw err;
+
+      if (result.length == 0)
+        res.send({"Success":false,"Message":"Incorrect Credentials"});
+      else
+      {
+        var token = jwt.sign(result[0], app.get('superSecret'), {
+          expiresInMinutes: 86400 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.send({
+          success: true,
+          token: token
+        });
+      }
+    });
+
+  });
+
+  apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+//Temporary API to get users
+  apiRoutes.get('/users', function(req, res) {
+    users.find().toArray(function(err,result)
+    {
+      if (err)
+        throw err;
+
+      if (result.length == 0)
+        res.send("Empty collection");
+      else
+        res.send(result);
+    });
+  });
+
+
+  app.use('/api', apiRoutes);
 
 });
